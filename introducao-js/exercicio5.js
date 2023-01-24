5ed70885197bca9ccb29ee6064b95fab3a4516f0
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-24 18:34:09 -0300

    add pastas java

--- introducao-js/exercicio5.js
+++ introducao-js/exercicio5.js
@@ -1,9 +1,14 @@
 const a = 45;
 const b = 45;
 const c = 90;
-if( a + b + c === 180){
-    console.log("True")
-}
-else {
-    console.log( "False")
+let soma = a + b + c;
+let todosangulospositivos = a > 0 && b > 0 && c > 0;
+if(todosangulospositivos){
+    if(soma === 180){
+    console.log(true);
+    } else {
+        console.log(false);
+    }   
+} else {
+    console.log("Erro: ângulo inválido");
 }
