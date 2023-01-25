af30abfb21fba4ff98a342a6ceda32cfe10d9319
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-25 18:32:30 -0300

    dia 3.3

--- javaScrip-array-loop-for/script6.js
+++ javaScrip-array-loop-for/script6.js
@@ -1,14 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
+const n = 9;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] % 2 !== 0) {
-    result += 1;
-  }
-}
-
-if (result === 0) {
-  console.log('nenhum valor ímpar encontrado');
-} else {
-  console.log(result);
-}
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
