2ec2bfed63920171fedb6df860fb6c02ebc27557
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-25 21:46:15 -0300

    exercicios

--- javaScript-3.3/exercicio1.js
+++ javaScript-3.3/exercicio1.js
@@ -1,6 +1,6 @@
-let fatorial = 10;
-let resultado = fatorial;
-for (let index = 1; index < fatorial; index += 1) {
-    resultado *= index;
+let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+    fatorial *= index;
 }
-console.log(resultado);
+console.log(fatorial);
---
+++ javaScript-3.3/exercicio2.js
@@ -0,0 +1,12 @@
+//let word = 'tryber';
+
+ //for (let reverse = str.length = 1; reverse-= 1) { 
+    //    word += str[reverse];        
+//}
+//console.log(word); 
+
+var word = "trybe";
+ 
+for (var index = word.length - 1; index >= 0; i--) {
+    console.log(word[index]);
+}
