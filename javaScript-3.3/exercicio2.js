8688438a5a67a00d9e0961b44a05a3fcbbf191ad
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-25 21:52:37 -0300

    exercicio 2

--- javaScript-3.3/exercicio2.js
+++ javaScript-3.3/exercicio2.js
@@ -1,12 +1,9 @@
-//let word = 'tryber';
+let word = 'tryber';
+let reverseWord = ''
 
- //for (let reverse = str.length = 1; reverse-= 1) { 
-    //    word += str[reverse];        
-//}
-//console.log(word); 
-
-var word = "trybe";
- 
-for (var index = word.length - 1; index >= 0; i--) {
-    console.log(word[index]);
+for (let index = 0; index < word.length; index += 1) {
+    reverseWord += word[word.length - 1 -index];
 }
+console.log(reverseWord);
+
+    
