eb0545a8da6b4395679982a0971480ab964f125f
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-27 10:36:39 -0300

    exercicio 5

---
+++ javaScript-3.4/exercicio5.js
@@ -0,0 +1,11 @@
+function maiorDeTres(primeiroNum, segundoNum, terceiroNum){
+    if ( primeiroNum > segundoNum && primeiroNum > terceiroNum){
+        return 'O maior número é: ' + primeiroNum;
+    } else if ( segundoNum  > primeiroNum && segundoNum > terceiroNum){
+    return 'O maior número é: ' + segundoNum;
+      }
+       else {
+        return 'O maior número é: ' + terceiroNum;
+    }
+}
+console.log(maiorDeTres(12,32,09))
