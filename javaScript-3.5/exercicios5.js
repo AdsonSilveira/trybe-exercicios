07cafab848bf25325cdc1ff7d812fe3ee9ea2246
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-27 21:36:18 -0300

    usando arrow function no exercicio 5

---
+++ javaScript-3.5/exercicios5.js
@@ -0,0 +1,6 @@
+//function numeroAleatorio() {
+   // return Math.random()
+  //}
+ // console.log(numeroAleatorio());
+  const numeroAleatorio = (Vasco) => Math.random();
+  console.log(numeroAleatorio())
