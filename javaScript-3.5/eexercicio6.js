b1cf5c68777a31b250d0ddefa26b519a3ca6920e
AdsonSilveira <adsonpancas80@gmail.com>
2023-01-27 22:05:04 -0300

    exercicio 6 de arrow function

---
+++ javaScript-3.5/eexercicio6.js
@@ -0,0 +1,9 @@
+//function hello(nome) {
+  //  return `Olá, ${nome}!`
+  //}
+  //let nome = 'Ivan';
+
+  const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome));
